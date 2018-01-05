b2fab5a099eaf8eb260fe103f843fa71545b9792
Elliot Laurendeau <laurendeau.elliot@gmail.com>
2018-01-04 21:11:40 -0500

    analog sensor for a light intensity

--- 3-light-sensor/main.go
+++ 3-light-sensor/main.go
@@ -4,20 +4,22 @@ import (
 	"gobot.io/x/gobot"
 	"gobot.io/x/gobot/drivers/aio"
 	"gobot.io/x/gobot/platforms/firmata"
+	"time"
 )
 
 func main() {
 
 	firmataAdapter := firmata.NewAdaptor("COM3")
-	lightSensor := aio.NewAnalogSensorDriver(firmataAdapter, "A0")
+	lightSensor := aio.NewAnalogSensorDriver(firmataAdapter, "0", 1*time.Second)
 
 
 	work := func() {
+
 		lightSensor.On(lightSensor.Event("data"), func(data interface{}) {
 			brightness := uint8(
 				gobot.ToScale(gobot.FromScale(float64(data.(int)), 0, 1024), 0, 255),
 			)
-			println(brightness)
+			println("Brightness: ", brightness)
 		})
 
 	}
@@ -27,5 +29,4 @@ func main() {
 		work)
 
 	robot.Start()
-
 }
